0acdb94bb4646d5c0fa4200949a2be94fa315ed3
GULLUNG Jennifer <jennifer.gullung@ynov.com>
2022-01-25 20:54:55 +0100

    add startup.cs

---
+++ GiftList/GiftList.API/Startup.cs
@@ -0,0 +1,72 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace GiftList.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly, Assembly.Load("GiftList.Application"));
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GiftList", Version = "v1" });
+            });
+
+            services.ConfigureDbContext(Configuration);
+            services.ConfigureIoC();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GiftList v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+    public static class ConfigurationsExtensions
+    {
+        public static IServiceCollection ConfigureIoC(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
